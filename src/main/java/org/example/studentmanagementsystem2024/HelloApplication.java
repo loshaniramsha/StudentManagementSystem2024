a3f13ab760507fb1d4766f588cf90f4f6a7e4f50
loshaniramsha <loshanikottagoda@gmail.com>
2024-06-30 01:31:58 +0530

    add

---
+++ src/main/java/org/example/studentmanagementsystem2024/HelloApplication.java
@@ -0,0 +1,9 @@
+package org.example.studentmanagementsystem2024;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
---
+++ src/main/java/org/example/studentmanagementsystem2024/StudentController.java
@@ -0,0 +1,45 @@
+package org.example.studentmanagementsystem2024;
+
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonReader;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+@WebServlet(urlPatterns = "/student")
+public class StudentController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //Todo:Save student
+        if (!req.getContentType().startsWith("application/json")) {
+            resp.setStatus(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE);
+        }
+    /*    BufferedReader reader=req.getReader();
+        StringBuilder sb=new StringBuilder();
+        var writer=resp.getWriter();
+        reader.lines().forEach(line->sb.append(line+"\n"));
+        System.out.println(sb);
+        writer.write(sb.toString());
+        writer.close();*/
+        // JSON mainpulate with parson
+        JsonReader reader= Json.createReader(req.getReader());
+        JsonObject jsonObject=reader.readObject();
+        System.out.println(jsonObject.getString("email"));
+
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doDelete(req, resp);
+    }
+}
